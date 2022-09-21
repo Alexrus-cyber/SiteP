08c7e31c05fe9724223b3728d1bb35d5238bc356
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-09-21 20:58:55 +0300

    NavBar(js and css) 1.2

--- react-portfolio/src/components/NavBar.js
+++ react-portfolio/src/components/NavBar.js
@@ -43,20 +43,20 @@ export const NavBar = () => {
                         <span className="navbar-toggler-icon"></span>
                     </Navbar.Toggle>
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto">
+                        <Nav >
                             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
                             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
                             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
                         </Nav>
                         <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://github.com/Alexrus-cyber"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.youtube.com/"><img src={navIcon2} alt="" /></a>
+                <a href="https://vk.com/arassadin2014"><img src={navIcon3} alt="" /></a>
               </div>
-              <HashLink to='#connect'>
+             {/* <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink>
+              </HashLink>*/}
             </span>
                     </Navbar.Collapse>
                 </Container>
